0a47896490f7046d97e61b8c22f0bee681ed35e9
Harshit Maheshwari <harshitmah98@gmail.com>
2019-08-08 23:36:27 +0530

    Added a basic theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+
+import './categories_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,9 +11,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+          title: TextStyle(fontSize: 20, fontFamily: "RobotoCondesed",fontWeight: FontWeight.bold,), 
+        ),
+
       ),
-      home: MyHomePage(title: 'DeliMeals'),
+      home: CategoriesScreen(),
     );
   }
 }
